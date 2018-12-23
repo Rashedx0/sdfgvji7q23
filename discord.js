d4935932644ccb9df1f0abef00cf8f17589c42b9
Rashedx0 <45704756+Rashedx0@users.noreply.github.com>
2018-12-23 22:28:58 +0300

    Update discord.js

--- discord.js
+++ discord.js
@@ -10,6 +10,30 @@ console.log("log");
 
 
 
+if (!message.member.roles.find("name", "@everyone")) { //Whatever role you want, I pick @everyone because everyone can use this command
+		message.channel.send('Invalid permissions.');
+		return;
+	}
+    
+    // Check for input
+    if (!args[0]) return message.channel.send('Proper usage: a.poll <question>');
+    
+    // Create Embed
+    const embed = new Discord.RichEmbed()
+        .setColor("#ffffff") //To change color do .setcolor("#fffff")
+        .setFooter('React to Vote.')
+        .setDescription(args.join(' '))
+        .setTitle(`Poll Created By ${message.author.username}`);
+        
+    let msg = await message.channel.send(embed)
+        .then(function (msg) {
+            msg.react("❎");
+            msg.react("✅"); // You can only add two reacts
+            message.delete({timeout: 1000});
+            }).catch(function(error) {
+            console.log(error);
+        });
+};
 
 
 
