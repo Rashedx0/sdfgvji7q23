3e977f79ea82949ef8b90e1222fce04e79dda244
Rashedx0 <45704756+Rashedx0@users.noreply.github.com>
2018-12-24 18:55:39 +0300

    Update discord.js

--- discord.js
+++ discord.js
@@ -9,25 +9,7 @@ console.log("log");
 
 
  
-if(message.content.startsWith(`${botSettings.prefix}setavatar`)) {
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0]
-    if(!command.startsWith(botSettings.prefix)) return;
-    const args = messageArray.slice(1);
-    let botmessage = args.join(" ");
-    if(message.author.id !== 'your id') return message.reply("Only my owner can use this!!");
 
-
-    let image = message.attachments.first().url;
-
-
-    bot.user.setAvatar(image);
-
-    let iEmbed = new Discord.RichEmbed()
-        .setAuthor('✅ Photo altered successfully')
-        .setColor('#1E90FF')
-        .addField("Your message 1", "your message 2")
-    message.channel.send(iEmbed)
 
 
 
